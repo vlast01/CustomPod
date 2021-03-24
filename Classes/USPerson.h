af4fe8ea0749da8b82b146124baddfb9ba5a437e
Vladislav <rct.stankevivs@gmail.com>
2021-03-24 11:15:31 +0300

    0.0.1

---
+++ Classes/USPerson.h
@@ -0,0 +1,18 @@
+//
+//  USPerson.h
+//  CustomPod
+//
+//  Created by Uladzislau Stankevich on 23.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface USPerson : NSObject
+
+@property (nonatomic, retain) NSString *name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/USPerson.m
@@ -0,0 +1,12 @@
+//
+//  USPerson.m
+//  CustomPod
+//
+//  Created by Uladzislau Stankevich on 23.03.21.
+//
+
+#import "USPerson.h"
+
+@implementation USPerson
+
+@end
